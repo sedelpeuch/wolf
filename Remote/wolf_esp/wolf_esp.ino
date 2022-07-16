f0655359b87b2b055862780849f08fe0f4d2426b
sedelpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2022-07-16 10:59:37 +0200

    ✨feat(esp32): add serializer json

--- Remote/wolf_esp/wolf_esp.ino
+++ Remote/wolf_esp/wolf_esp.ino
@@ -4,9 +4,11 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <LiquidCrystal.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "eirlabIoT";
-const char* password = "eirlab2021enseirb!";
+const char* password = "";
+const char* token = "";
 
 // Create An LCD Object. Signals: [ RS, EN, D4, D5, D6, D7 ]
 LiquidCrystal My_LCD(13, 12, 14, 27, 26, 25);
@@ -32,7 +34,7 @@ void setup() {
   pinMode(BUZZ_PIN, OUTPUT);
   digitalWrite(BUZZ_PIN, LOW);
 
-  
+
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
@@ -45,7 +47,7 @@ void setup() {
   My_LCD.clear();
 
   My_LCD.print("Initialize");
-  
+
  //Init Serial USB
  Serial.begin(115200);
  Serial.println(F("Initialize System"));
@@ -56,7 +58,7 @@ void setup() {
  rfid.PCD_DumpVersionToSerial();
 
   delay(1000);
-  
+
   My_LCD.clear();
   My_LCD.print("Connecting");
   My_LCD.setCursor(0,1);
@@ -71,7 +73,7 @@ void setup() {
     delay(100);
   }
 
-  
+
   My_LCD.clear();
   My_LCD.print("Connected");
   My_LCD.setCursor(0,1);
@@ -82,11 +84,11 @@ void setup() {
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
 
-  digitalWrite(RED_PIN, LOW);  
+  digitalWrite(RED_PIN, LOW);
   digitalWrite(BLUE_PIN, HIGH);
 
 
-  
+
   My_LCD.clear();
 }
 void loop() {
@@ -94,8 +96,6 @@ void loop() {
  My_LCD.print("Pret a scanner");
  delay(100);
  readRFID(&uid);
-
- 
 }
 
 
@@ -118,7 +118,7 @@ void readRFID(String *uid) { /* function readRFID */
  *uid = Uid_Hex(rfid.uid.uidByte, rfid.uid.size);
 
   Serial.println(*uid);
- 
+
   digitalWrite(BLUE_PIN, LOW);
   digitalWrite(BUZZ_PIN, HIGH);
   digitalWrite(GREEN_PIN, HIGH);
@@ -127,62 +127,94 @@ void readRFID(String *uid) { /* function readRFID */
   delay(500);
  // Halt PICC
 
-  
+
   digitalWrite(BLUE_PIN, LOW);
   digitalWrite(RED_PIN, HIGH);
 
-  
+
   My_LCD.clear();
   My_LCD.print("ID: ");
   for(int i = 0; i < 12; i++)
   {
-    My_LCD.print((*uid)[i]);    
+    My_LCD.print((*uid)[i]);
   }
   My_LCD.setCursor(0,1);
   for(int i = 12; i < 20; i++)
   {
-    My_LCD.print((*uid)[i]);    
+    My_LCD.print((*uid)[i]);
   }
- 
+
  rfid.PICC_HaltA();
  // Stop encryption on PCD
  rfid.PCD_StopCrypto1();
 
  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
-  
     HTTPClient http;
-  
-    http.begin("https://gestion.eirlab.net/api/index.php/members?sortfield=t.rowid&sortorder=ASC&limit=600"); //Specify the URL
-    http.addHeader("Accept", "application/json");
-    http.addHeader("DOLAPIKEY", "sPfJjheCExeL");
-    int httpCode = http.GET();                                        //Make the request
-  
-    if (httpCode > 0) { //Check for the returning code
-  
-        String payload = http.getString();
-        Serial.println(httpCode);
-        Serial.println(payload);
+    for(int j=1; j<20; j++){
+      String j_str = (String) j;
+      My_LCD.clear();
+      My_LCD.print("Recherche en cours ...");
+      http.begin("https://gestion.eirlab.net/api/index.php/members/?sortfield=t.rowid&sortorder=ASC&limit=20&page="+j_str); //Specify the URL
+      http.addHeader("Accept", "application/json");
+      http.addHeader("DOLAPIKEY", token);
+      int httpCode = http.GET();
+      if (httpCode > 0 ) { //Check for the returning code
+        DynamicJsonDocument doc(50000);
+        String json = http.getString();
 
-        
+        DeserializationError error = deserializeJson(doc, json);
+
+        if (error) {
+          Serial.print("deserializeJson() failed: ");
+          Serial.println(error.c_str());
+          return;
+        }
+
+        for (JsonObject item : doc.as<JsonArray>()) {
+
+          const char* id = item["id"]; // "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", ...
+          const char* lastname = item["lastname"]; // "FRAYSSINOUS", "Amat", "Mongabure", "PARAISO", "Darmon", ...
+          const char* firstname = item["firstname"]; // "Guy", "Paul", "Xan", "Aurèle", "Valentin", "Fatima", ...
+          long datec = item["datec"]; // 1647471600, 1647385200, 1647212400, 1646694000, 1646262000, 1646262000, ...
+          long datem = item["datem"]; // 1657384213, 1648315351, 1648315351, 1648315351, 1648315351, 1648315351, ...
+          String nserie = item["array_options"]["options_nserie"];
+          if(nserie == *uid){
+            My_LCD.clear();
+            My_LCD.print(lastname);
+            Serial.println(lastname);
+            Serial.println(firstname);
+            delay(5000);
+
+            My_LCD.clear();
+
+            digitalWrite(RED_PIN, LOW);
+            digitalWrite(GREEN_PIN, LOW);
+            digitalWrite(BLUE_PIN, HIGH);
+            return;
+          }
+        }
       }
-  
-    else {
-      Serial.println("Error on HTTP request");
+
+      else {
+        Serial.println("Error on HTTP request");
+      }
+
+      http.end(); //Free the resources
+      My_LCD.clear();
+      My_LCD.print("Adhérent inconnu");
     }
-  
-    http.end(); //Free the resources
   }
 
 
-  
+
   delay(5000);
-  
+
   My_LCD.clear();
-  
+
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, LOW);
   digitalWrite(BLUE_PIN, HIGH);
-  
+
 }
 /**
    Helper routine to dump a byte array as hex values to Serial.
@@ -193,7 +225,7 @@ String Uid_Hex(byte *buffer, byte bufferSize) {
  for (byte i = 0; i < bufferSize; i++) {
   if(i != bufferSize - 1)
    {
-   sprintf(provi, "%s%X:", buffer[i] < 0x10 ? "0" : "", buffer[i]);
+   sprintf(provi, "%s%X:", buffer[i] < 0x10 ? "" : "", buffer[i]);
    }
    else
    {
