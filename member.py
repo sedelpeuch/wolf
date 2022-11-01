c0d800708b52f54b69dea5d863252000cba36c27
sedelpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2022-11-01 14:48:53 +0100

    feat(member): Add list all member

--- member.py
+++ member.py
@@ -26,6 +26,7 @@ class Member:
         self.bp.route('/scan')(self.scan_member)
         self.bp.route('/new_link', methods=['POST'])(self.new_link)
         self.bp.route('/confirm_link')(self.confirm_link)
+        self.bp.route('/list', methods=['POST'])(self.list_member)
 
         self.data = None  # Données provenant des requêtes dolibarr sur les adhérents
         self.actual_n_serie = None  # Numéro de série de la dernière carte RFID lue
@@ -36,9 +37,12 @@ class Member:
         """
         Permet de lire la carte RFID et de demander la correspondance à la base de données sur dolibarr
 
-        :return: index.html avec le message d'erreur "Connectez le PC à internet" si la connexion à la base de données n'est pas établie
-        :return: index.html avec le message d'erreur "Connectez le lecteur RFID et réessayez" si la lecture de la carte n'est pas possible
-        :return: index.html avec le message d'erreur "Adhérent inconnu" si l'adhérent n'est pas trouvé, l'onglet "Adhérent inconnu" avec le formulaire nom prénom apparait
+        :return: index.html avec le message d'erreur "Connectez le PC à internet" si la connexion à la base de
+        données n'est pas établie
+        :return: index.html avec le message d'erreur "Connectez le lecteur RFID et réessayez" si la lecture de la
+        carte n'est pas possible
+        :return: index.html avec le message d'erreur "Adhérent inconnu" si l'adhérent n'est pas trouvé,
+        l'onglet "Adhérent inconnu" avec le formulaire nom prénom apparait
         :return: index.html avec la fiche de l'adhérent si l'adhérent est trouvé
         """
         status = self.rfid.initialize()
@@ -64,10 +68,13 @@ class Member:
         Permet de lier une carte RFID à un adhérent à partir de son nom et prénom, l'onglet "Adhérent inconnu" est
         accessible uniquement après avoir scanné une carte (fonction scan_member)
 
-        :return: index.html avec le message d'erreur "Connectez le PC à internet" si la connexion à la base de données n'est pas établie
+        :return: index.html avec le message d'erreur "Connectez le PC à internet" si la connexion à la base de
+        données n'est pas établie
         :return: index.html avec le message "Non adhérent" si l'adhérent n'est pas trouvé
-        :return: index.html avec le message d'erreur "Adhérent déjà lié" si la carte RFID est déjà liée à un adhérent, la fiche de l'adhérent apparait
-        :return: index.html avec le message d'erreur "Adhérent non lié" si la carte RFID n'est pas liée, la fiche de l'adhérent apparait
+        :return: index.html avec le message d'erreur "Adhérent déjà lié" si la carte RFID est déjà liée à un
+        adhérent, la fiche de l'adhérent apparait
+        :return: index.html avec le message d'erreur "Adhérent non lié" si la carte RFID n'est pas liée, la fiche de
+        l'adhérent apparait
         """
         lastname = request.form['lastname']
         lastname = unidecode.unidecode(lastname)
@@ -143,3 +150,32 @@ class Member:
             return render_template(template_name_or_list='index.html', status='Adhérent non lié', new=True,
                                    member=self.actual_member, error=True, lastname=member["lastname"],
                                    firstname=member["firstname"])
+
+    def list_member(self):
+        """
+        Permet d'afficher la liste des adhérents
+
+        :return: index.html avec la liste des adhérents
+        """
+        try:
+            r = requests.get(config.url + config.url_member, headers=config.headers)
+        except requests.ConnectionError:
+            return render_template(template_name_or_list='index.html', status='Connectez le PC à internet')
+        self.data = json.loads(r.text)
+        for member in self.data:
+            member = common.process_member(member)
+
+        result_search = []
+
+        if request.form['lastname'] != "" or request.form['firstname'] != "":
+            for member in self.data:
+                if member['lastname'].lower() == request.form['lastname'].lower():
+                    result_search.append(member)
+                elif member['firstname'].lower() == request.form['firstname'].lower():
+                    result_search.append(member)
+            result_search = sorted(result_search, key=lambda k: k['lastname'])
+            return render_template(template_name_or_list='index.html', list_member=result_search)
+
+        # sort self.data by firstname
+        self.data = sorted(self.data, key=lambda k: k['lastname'])
+        return render_template(template_name_or_list='index.html', list_member=self.data)
