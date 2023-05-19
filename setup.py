701615e64cdc026e4d965ff555900b35f79ec9b9
Sébastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2023-05-19 16:52:36 +0200

    feat(config): update repo structure

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='wolf', version='1.1.0', description='', author='sedelpeuch', author_email='sebastien@delpeuch.net', packages=find_packages(),
-      install_requires=[  # List your project dependencies here
-      ], entry_points={'console_scripts': ['wolf = wolf.__main__:main']},
-      classifiers=['Development Status :: 3 - Alpha', 'License :: OSI Approved :: GPL3 License', 'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: 3.9', ], )
