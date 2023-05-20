19e150ef64ceee8a175454560ef1af2ed91104e7
Sébastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2023-05-20 11:30:32 +0200

    feat(core): Update interface structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../core/core/'))
+sys.path.insert(0, os.path.abspath('../../core/wolf_core/'))
 
 
 project = 'Wolf'
---
+++ wolf/main.py
@@ -0,0 +1,6 @@
+from wolf_core import runner
+import sync_calendar
+
+if __name__ == "__main__":
+    main_runner = runner.Runner(debug=True)
+    main_runner.run()
