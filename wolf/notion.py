8aab577d2858ce251a5d551cebc93214b338f7a3
Sébastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2023-07-18 22:38:54 +0200

    feat(notion): Add Notion API interaction class
    This commit adds a `Notion` class under `wolf/notion.py` which is a subclass of existing `API`. The `Notion` class is designed to interact with the Notion API and includes methods for authenticating using OAuth and methods for fetching databases and pages. This makes it easier to integrate and use the Notion API within the application. Added functionality for retrieving database and pages information by their ID.

---
+++ wolf/notion.py
@@ -0,0 +1,72 @@
+"""
+This module contains the Notion class, which is a subclass of the API class. It is used to interact with the Notion API.
+The Notion API is documented here: https://developers.notion.com/reference/intro
+"""
+
+# 286ecfe40ac34190960ca136e54901a8
+
+import json
+
+import requests
+
+from wolf_core import api
+
+
+class Notion(api.API):
+    def __init__(self):
+        with open("token.json") as f:
+            token = json.load(f)["notion"]
+
+        ressources = {
+            "databases": {"verb": "GET", "method": self.get_databases, "params": str},
+            "pages": {"verb": "GET", "method": self.get_pages, "params": str},
+        }
+
+        super().__init__(url="https://api.notion.com", test_url="", token=token, ressources=ressources)
+
+    def oauth_header(self):
+        """
+        Generate the OAuth header for API requests.
+
+        :return: The OAuth header.
+        :rtype: dict
+        """
+        return {
+            "Authorization": f"Bearer {self._token}",
+            "Notion-Version": "2022-06-28",
+        }
+
+    def get_databases(self, params):
+        """
+        Retrieves information about a Notion database.
+
+        :param params: The ID of the database.
+        :return: A RequestResponse object containing the status code and data of the API request.
+        """
+
+        url = self._url + "/v1/databases/" + params
+        header = self.oauth_header()
+        response = requests.get(url, headers=header)
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
+    def get_pages(self, params):
+        """
+        Retrieve pages from Notion API.
+
+        :param params: The parameters used to retrieve the pages.
+        :return: The response containing the pages' data.
+
+        """
+        url = self._url + "/v1/pages/" + params
+        header = self.oauth_header()
+        header["accept"] = "application/json"
+        response = requests.get(url, headers=header)
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
+
+if __name__ == "__main__":
+    notion = Notion()
+    print(notion.get.databases("str").status_code)
+    print(notion.get.pages("str").status_code)
