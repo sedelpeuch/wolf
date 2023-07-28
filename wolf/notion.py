c612f392e77d00e9ae145c61ba61094d894dbf6b
Sébastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2023-07-28 14:24:35 +0200

    Expand Notion API functionality in Notion class
    Updated the Notion class with comprehensive database, user, and search methods. More specifically, methods for creating, querying, and patching databases were added. Furthermore, the method for getting pages has been refined to fetch a particular page. User-related methods and mechanisms to search pages were included. The Notion token was also updated to be set as an environment variable for better security.
    
    The new methods allow users to access more features available in the Notion API, leading to a more robust and interactive user experience within Notion via the API.

--- wolf/notion.py
+++ wolf/notion.py
@@ -12,15 +12,25 @@ from wolf_core import api
 
 
 class Notion(api.API):
+    """
+    The reference is your key to a comprehensive understanding of the Notion API.
+
+    Integrations use the API to access Notion's pages, databases, and users.
+    Integrations can connect services to Notion and build interactive experiences for users within Notion.
+    Using the navigation on the left, you'll find details for objects and endpoints used in the API.
+
+    https://developers.notion.com/reference/intro
+    """
+
     def __init__(self):
         absolute_path = os.path.dirname(os.path.abspath(__file__))
         with open(os.path.join(absolute_path, "token.json")) as f:
             token = json.load(f)["notion"]
+            os.environ["NOTION_TOKEN"] = token
 
         ressources = {
-            "databases": {"verb": "GET", "method": self.get_databases, "params": str},
             "pages": {
-                "page_get": {"verb": "GET", "method": self.get_pages, "params": str},
+                "page_get": {"verb": "GET", "method": self.get_page, "params": str},
                 "page_post": {"verb": "POST", "method": self.post_page, "params": [str, dict],
                               "optional_params": [list, dict, dict, dict]}},
             "page_property": {
@@ -34,7 +44,18 @@ class Notion(api.API):
             "block": {
                 "block_patch": {"verb": "PATCH", "method": self.update_block, "params": [str, dict]},
                 "block_delete": {"verb": "DELETE", "method": self.delete_block, "params": str},
-                "block_get": {"verb": "GET", "method": self.get_block, "params": str}}
+                "block_get": {"verb": "GET", "method": self.get_block, "params": str}},
+            "create_database": {"verb": "POST", "method": self.create_database, "params": [str, dict],
+                                "optional_params": str},
+            "database": {
+                "database_get": {"verb": "GET", "method": self.get_database, "params": str},
+                "database_patch": {"verb": "PATCH", "method": self.patch_database, "params": str,
+                                   "optional_params": [list, list, dict]},
+                "database_query": {"verb": "POST", "method": self.query_database, "params": str,
+                                   "optional_params": [dict, list, str, int]}
+            },
+            "user": {"verb": "GET", "method": self.get_user, "params": "", "optional_params": str},
+            "search": {"verb": "POST", "method": self.search, "params": dict},
         }
 
         super().__init__(url="https://api.notion.com", test_url="", token=token, ressources=ressources)
@@ -112,7 +133,7 @@ class Notion(api.API):
         :param archived: Boolean value indicating whether the block should be archived or not.
         :param block_id: The ID of the block to update.
         :param data: The data to update the block with.
-        :return: The response containing the block's data.
+        :return: The response contains the block's data.
         :rtype: api.RequestResponse
         """
         url = self._url + "/v1/blocks/" + block_id
@@ -167,7 +188,7 @@ class Notion(api.API):
         request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
         return request_response
 
-    def get_pages(self, params):
+    def get_page(self, params):
         """
         Retrieve pages from Notion API.
 
@@ -228,6 +249,179 @@ class Notion(api.API):
         request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
         return request_response
 
+    ##################################
+    # Database: Notion API requests #
+    ##################################
+
+    def create_database(self, parent, properties, title=None):
+        """
+        Creates a database as a subpage in the specified parent page, with the specified properties' schema.
+        Currently, the parent of a new database must be a Notion page.
+
+        :param parent: The ID of the parent page.
+        :param properties: The properties of the database.
+        :param title: (optional) The title of the database.
+        :return: The response contains the database's data.
+        :rtype: api.RequestResponse
+        """
+        url = self._url + "/v1/databases"
+        header = self.oauth_header()
+        header["Content-Type"] = "application/json"
+        data = {
+            "parent": {"page_id": parent},
+            "properties": properties,
+            "title": title
+        }
+        response = requests.post(url, headers=header, data=json.dumps(data))
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
+    def query_database(self, id, filter=None, sorts=None, start_cursor=None, page_size=None):
+        """
+        Gets a list of Pages contained in the database, filtered and ordered according to the filter conditions and
+        sort criteria provided in the request.
+        The response may contain fewer than page_size of results.
+        If the response includes a next_cursor value, refer to the pagination reference for details about how to use a
+        cursor to iterate through the list.
+
+        Filters are similar to the filters provided in the Notion UI,
+        where the set of filters and filter groups chained by "And"
+        in the UI is equivalent to having each filter in the array of the compound "and" filter.
+        The same set of filters chained by "Or" in the UI would be represented as filters in the array of the "or"
+        compound filter.
+        Filters operate on database properties and can be combined.
+        If no filter is provided, all the pages in the database will be returned with pagination.
+
+        :param id: The ID of the database.
+        :param filter: (optional) The filter of the database.
+        :param sorts: (optional) The sorts of the database.
+        :param start_cursor: (optional) The start cursor of the database.
+        :param page_size: (optional) The page size of the database.
+        :return: The response contains the database's data.
+        :rtype: api.RequestResponse
+        """
+        url = self._url + "/v1/databases/" + id + "/query"
+        header = self.oauth_header()
+        header["Content-Type"] = "application/json"
+        data = {
+            "filter": filter,
+            "sorts": sorts,
+            "start_cursor": start_cursor,
+            "page_size": page_size
+        }
+        response = requests.post(url, headers=header, data=json.dumps(data))
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
+    def get_database(self, id):
+        """
+        Retrieves a database object — information that describes the structure and columns of a database —
+        for a provided database ID. The response adheres to any limits to an integration’s capabilities.
+
+        To fetch database rows rather than columns, use the "Query a database endpoint."
+
+        :param id: The ID of the database.
+        :return: A RequestResponse object containing the status code and data of the API request.
+        """
+
+        url = self._url + "/v1/databases/" + id
+        header = self.oauth_header()
+        response = requests.get(url, headers=header)
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
+    def patch_database(self, id, title=None, description=None, properties=None):
+        """
+        Updates the database object — the title, description, or properties — of a specified database.
+
+        Returns the updated database object.
+
+        Database properties represent the columns (or schema) of a database.
+        To update the properties of a database, use the properties body param with this endpoint.
+        Learn more about database properties in the database properties and Update database properties docs.
+
+        To update a relation database property, share the related database with the integration.
+        Learn more about relations on the database properties page.
+
+        For an overview of how to use the REST API with databases, refer to the Working with databases guide.
+
+        :param id: The ID of the database.
+        :param title: (optional) The title of the database.
+        :param description: (optional) The description of the database.
+        :param properties: (optional) The properties of the database.
+        :return: A RequestResponse object containing the status code and data of the API request.
+        """
+
+        url = self._url + "/v1/databases/" + id
+        header = self.oauth_header()
+        header["Content-Type"] = "application/json"
+        data = {
+            "title": title,
+            "description": description,
+            "properties": properties
+        }
+        response = requests.patch(url, headers=header, data=json.dumps(data))
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
+    ##############################
+    # Users: Notion API requests #
+    ##############################
+
+    def get_user(self, id=None):
+        """
+        Retrieves a user object using the ID specified in the request URL if id is specified,
+        or retrieves all users that the integration has access to if id is not specified.
+
+        :param id: The ID of the user.
+        :return: A RequestResponse object containing the status code and data of the API request.
+        """
+
+        if id is None:
+            url = self._url + "/v1/user"
+        else:
+            url = self._url + "/v1/users/" + id
+        header = self.oauth_header()
+        response = requests.get(url, headers=header)
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
+    ###############################
+    # Search: Notion API requests #
+    ###############################
+
+    def search(self, query, sort=None, filter=None, start_cursor=None, page_size=None):
+        """
+        Searches all pages and child pages that are shared with the integration.
+        The response may contain fewer than page_size of results.
+        If the response includes a next_cursor value, refer to the pagination reference for details about how to use a
+        cursor to iterate through the list.
+
+        :param query: The query of the search.
+        :param sort: (optional) The sort of the search.
+        :param filter: (optional) The filter of the search.
+        :param start_cursor: (optional) The start cursor of the search.
+        :param page_size: (optional) The page size of the search.
+        :return: A RequestResponse object containing the status code and data of the API request.
+        """
+
+        url = self._url + "/v1/search"
+        header = self.oauth_header()
+        header["Content-Type"] = "application/json"
+        data = {
+            "query": query,
+            "sort": sort,
+            "filter": filter,
+            "start_cursor": start_cursor,
+            "page_size": page_size
+        }
+        response = requests.post(url, headers=header, data=json.dumps(data))
+        request_response = api.RequestResponse(status_code=response.status_code, data=response.json())
+        return request_response
+
 
 if __name__ == "__main__":
     notion = Notion()
+    req = notion.get.block_children("011f57a503864878b9e777d80932706d")
+    print(req.status_code)
+    print(req.data)
