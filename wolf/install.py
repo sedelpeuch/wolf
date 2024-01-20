7ea3dcc686552b7bcd319156c03d8ace23f9abb7
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-01-20 15:26:09 +0100

    Refactor token handling in install.py script
    The token handling in 'wolf/install.py' has been refactored for improved code clarity and efficiency. The function argument name 'token_dict' has been altered to 'token_data' for better descriptive accuracy. Redundant print statements and comments have been removed for a cleaner and more maintainable codebase. Argparse is now utilized for more generic token handling.

--- wolf/install.py
+++ wolf/install.py
@@ -2,7 +2,8 @@
 """
 Create_token_file(token_dict, file_path):
 This function writes a provided dictionary (token_dict) into a JSON file at
-file_path (defaults to 'token.json' if not specified). If the file already exists, its content is replaced with the
+file_path (defaults to 'token.json' if not specified). If the file already exists, its content
+is replaced with the
 new token_dict.
 """
 import argparse
@@ -12,49 +13,47 @@ import os
 import subprocess
 
 
-def create_token_file(token_dict, file_path="token.json"):
+def create_token_file(token_data, file_path="token.json"):
     """
     Create a token file.
 
-    :param token_dict: A dictionary containing tokens.
+    :param token_data: A dictionary containing tokens.
     :param file_path: Optional. The path of the token file. The default is 'token.json.'
     :return: None
     """
     if not os.path.exists(file_path):
         subprocess.run(["touch", file_path])
     with open(file_path, "w") as token_file:
-        json.dump(token_dict, token_file)
+        json.dump(token_data, token_file)
     print(f"token.json created at {file_path}.")
 
 
 def parse_arguments():
+    """
+    Parses command line arguments and returns the parsed arguments.
+
+    :return: A tuple containing the parsed arguments.
+    """
     parser = argparse.ArgumentParser()
-    # parse argument user
     parser.add_argument(
         "--user", type=str, default=getpass.getuser(), help="user name"
     )
-    # You can specify some known arguments, e.g., service_name in this case
     unknown_args = parser.parse_known_args()
-
-    # Convert unknown args to token dictionary
-    token_dict = {}
+    user_args = {}
     for arg in unknown_args[1]:
         if arg.startswith("--"):
             key = arg[2:]
             value = unknown_args[1][unknown_args[1].index(arg) + 1]
-            token_dict[key] = value
+            user_args[key] = value
 
-    return unknown_args[0], token_dict
+    return unknown_args[0], user_args
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    # check if args is not empty
     if args[1]:
         token_dict = args[1]
     else:
         # Needs to be replaced with the actual token. KEEP SECRET AND NEVER COMMIT TO GIT!
         token_dict = {"app": "token"}
-    print(token_dict)
-
     create_token_file(token_dict)
